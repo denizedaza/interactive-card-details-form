d06dc13257eaccb5b9a452aeaac0298983f41551
denizedaza <dazadenize@gmail.com>
2022-08-24 20:14:28 -0400

    start on form layout

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,11 +1,32 @@
 import Cards from "./Cards";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 
 function CardDetails() {
   return (
-    <div>
+    <Box>
       <Cards />
-      <h1>Hello, the CardDetails component!</h1>
-    </div>
+      <FormControl>
+        <FormLabel>Cardholder Name</FormLabel>
+        <Input placeholder="e.g Jane Appleseed" type="text" mb="24px" />
+        <FormLabel>Card Number</FormLabel>
+        <Input type="number" placeholder="e.g. 1234 5678 9123 0000" />
+        <FormLabel>Expiry Date (MM/YY)</FormLabel>
+        <Input type="number" placeholder="MM" />
+        <Input type="number" placeholder="YY" />
+        <FormLabel>CVC</FormLabel>
+        <Input type="number" placeholder="e.g. 123" />
+
+        <Button>Confirm</Button>
+      </FormControl>
+    </Box>
   );
 }
 
