9901c3a47f529c923c1c42d7417f71f14f067a97
denizedaza <dazadenize@gmail.com>
2022-08-26 17:44:38 -0400

    fix paths for font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500\&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500&display=swap"
             rel="stylesheet"
           />
         </Head>
