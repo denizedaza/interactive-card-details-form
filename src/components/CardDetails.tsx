119f53b85f5b495e7359fbb7db75bd93eaf81e96
denizedaza <dazadenize@gmail.com>
2022-08-25 22:12:56 -0400

    add proper grid for form

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,11 +1,15 @@
 import Cards from "./Cards";
 import {
   Box,
+  Container,
   FormControl,
   FormLabel,
   FormErrorMessage,
   FormHelperText,
+  VStack,
   Input,
+  SimpleGrid,
+  GridItem,
   Button,
 } from "@chakra-ui/react";
 
@@ -13,19 +17,37 @@ function CardDetails() {
   return (
     <Box>
       <Cards />
-      <FormControl>
-        <FormLabel>Cardholder Name</FormLabel>
-        <Input placeholder="e.g Jane Appleseed" type="text" mb="24px" />
-        <FormLabel>Card Number</FormLabel>
-        <Input type="number" placeholder="e.g. 1234 5678 9123 0000" />
-        <FormLabel>Expiry Date (MM/YY)</FormLabel>
-        <Input type="number" placeholder="MM" />
-        <Input type="number" placeholder="YY" />
-        <FormLabel>CVC</FormLabel>
-        <Input type="number" placeholder="e.g. 123" />
-
-        <Button>Confirm</Button>
-      </FormControl>
+      <Container bg="red.400" centerContent transform="translate(25%, 50%)">
+        <VStack w="full" h="full" p={10} spacing="24px">
+          <FormControl>
+            <FormLabel>Cardholder Name</FormLabel>
+            <Input placeholder="e.g Jane Appleseed" type="text" />
+            <FormLabel>Card Number</FormLabel>
+            <Input type="number" placeholder="e.g. 1234 5678 9123 0000" />
+            <SimpleGrid columns={2} columnGap={3} rowGap={2}>
+              <GridItem colSpan={1}>
+                <FormLabel>Expiry Date (MM/YY)</FormLabel>
+                {/* separate dates into smaller grid */}
+                <SimpleGrid columns={2}>
+                  <GridItem colSpan={1}>
+                    <Input type="number" placeholder="MM" />
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <Input type="number" placeholder="YY" />
+                  </GridItem>
+                </SimpleGrid>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <FormLabel>CVC</FormLabel>
+                <Input type="number" placeholder="e.g. 123" />
+              </GridItem>
+            </SimpleGrid>
+            <Button colorScheme="blackAlpha" size="lg" w="full">
+              Confirm
+            </Button>
+          </FormControl>
+        </VStack>
+      </Container>
     </Box>
   );
 }
