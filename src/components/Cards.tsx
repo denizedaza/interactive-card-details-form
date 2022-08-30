e044ca82fa9305afe9a5fb581eed915041e7a97c
denizedaza <dazadenize@gmail.com>
2022-08-29 21:54:35 -0400

    layout text for front of card

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,17 @@
 import cardBack from "/images/bg-card-back.png";
 import cardFront from "/images/bg-card-front.png";
-import { Box, Container, Icon, Text } from "@chakra-ui/react";
+import cardLogo from "/images/card-logo.svg";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import { CardInfo } from "../interfaces/index";
-import { CardLogo, CompleteIcon } from "../icons/Icon";
+import { CardLogo } from "../icons/Icon";
 
 function CardDisplay(props: CardInfo) {
   const { name, number, expDateMonth, expDateYear, cvcNumber } = props;
@@ -11,11 +20,28 @@ function CardDisplay(props: CardInfo) {
     <Box pos="absolute" ml="-265px" mt="166px">
       <Container>
         <Box ml="-100px" boxShadow="2xl">
-          <Icon as={CardLogo} pos="absolute" />
+          <Box pos="absolute" p={6}>
+            <img src={cardLogo.src} />
+            <Flex direction="column" gap="4" color="white" mt="52px">
+              <Heading as="h3" size="xl" fontSize="35px" color="white">
+                {number}
+              </Heading>
+              <Flex>
+                <Text as="p">{name}</Text>
+                <Spacer />
+                <Text as="p">
+                  {expDateMonth}/{expDateYear}
+                </Text>
+              </Flex>
+            </Flex>
+          </Box>
+          {/* base card */}
           <img src={cardFront.src} className="object-none" />
         </Box>
         <Box mt="36px" boxShadow="2xl">
-          <Text>{cvcNumber}</Text>
+          <Box pos="absolute">
+            <Text color="white">{cvcNumber}</Text>
+          </Box>
           <img src={cardBack.src} className="object-none" />
         </Box>
       </Container>
