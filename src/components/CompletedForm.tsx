e178b4302b671116f0da6507f826fa556eb12884
denizedaza <dazadenize@gmail.com>
2022-09-01 14:17:49 -0400

    create page after submitting

---
+++ src/components/CompletedForm.tsx
@@ -0,0 +1,23 @@
+import { Container, Heading, Text, Button, VStack } from "@chakra-ui/react";
+import completeLogo from "/images/icons/complete.svg";
+
+function CompletedForm() {
+  return (
+    <Container
+      centerContent
+      transform="translate(40%, 53%)"
+      alignItems="flex-start"
+    >
+      <VStack>
+        <img src={completeLogo} alt="complete" />
+        <Heading>Thank you!</Heading>
+        <Text>We've added your card details</Text>
+        <Button colorScheme="blackAlpha" bg="black" size="lg" w="full" mt={8}>
+          Continue
+        </Button>
+      </VStack>
+    </Container>
+  );
+}
+
+export default CompletedForm;
