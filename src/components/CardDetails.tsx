5e3468c8aca497625ed27dacd805c396efa2fcc5
denizedaza <dazadenize@gmail.com>
2022-09-22 17:18:21 -0400

    refactor: split form of card into own component

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,30 +1,13 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
-import Cards from "./Cards";
-import {
-  Box,
-  Container,
-  FormControl,
-  FormLabel,
-  VStack,
-  Input,
-  SimpleGrid,
-  GridItem,
-  Button,
-} from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useFormContext } from "react-hook-form";
 
-import ThankYouPage from "./ThankYouPage";
+import Cards from "./Cards";
 import { CardInfo } from "../interfaces/index";
-import styles from "./CardDetails.module.css";
+import CardForm from "./CardForm";
 
 function CardDetails() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<CardInfo>();
   const defaultCardInfo: CardInfo = {
     name: "Jane Appleseed",
     number: "0000 0000 0000 0000",
@@ -37,7 +20,11 @@ function CardDetails() {
   const [cardInfo, setCardInfo] = useState<CardInfo>(defaultCardInfo);
 
   function formatCardNumber(string: string): string {
-    if (string.includes(" ", 4)) {
+    if (
+      string.includes(" ", 4) &&
+      string.includes(" ", 9) &&
+      string.includes(" ", 14)
+    ) {
       return string;
     }
     let formattedCardNumber: string = "";
@@ -55,7 +42,7 @@ function CardDetails() {
     return formattedCardNumber;
   }
 
-  function onSubmit(values: CardInfo) {
+  function handleSubmit(values: CardInfo) {
     const newCardInfo = {
       name: values.name,
       number: formatCardNumber(values.number),
@@ -64,11 +51,7 @@ function CardDetails() {
       cvcNumber: values.cvcNumber,
     };
     setCardInfo(newCardInfo);
-    router.push("/thank-you");
-  }
-
-  function checkIfInvalid(value: string): boolean {
-    return Object.hasOwn(errors, value);
+    // router.push("/thank-you");
   }
 
   return (
@@ -82,139 +65,10 @@ function CardDetails() {
       />
       <Container
         centerContent
-        transform="translate(40%, 53%)"
+        transform="translate(36%, 50%)"
         alignItems="flex-start"
       >
-        <VStack w="full" h="full" p={10} spacing="24px">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl>
-              <FormLabel>Cardholder Name</FormLabel>
-              <Input
-                placeholder="e.g Jane Appleseed"
-                type="text"
-                {...register("name", {
-                  required: "Please enter your name",
-                })}
-                isInvalid={checkIfInvalid("name")}
-              />
-              <ErrorMessage
-                errors={errors}
-                name="name"
-                render={({ message }) => (
-                  <p className={styles.error}>{message}</p>
-                )}
-              />
-              <FormLabel mt={5}>Card Number</FormLabel>
-              <Input
-                type="text"
-                placeholder="e.g. 1234 5678 9123 0000"
-                {...register("number", {
-                  required: "Can't be blank",
-                  pattern: {
-                    // value: /^[0-9]*\s*$/,
-                    value: /\d\s*/,
-                    message: "Wrong format, numbers only",
-                  },
-                  minLength: {
-                    value: 16,
-                    message: "Number is too short",
-                  },
-                  maxLength: 19,
-                })}
-                isInvalid={checkIfInvalid("number")}
-              />
-              <ErrorMessage
-                errors={errors}
-                name="number"
-                render={({ message }) => (
-                  <p className={styles.error}>{message}</p>
-                )}
-              />
-              <SimpleGrid columns={2} columnGap={6} rowGap={2}>
-                <GridItem colSpan={1}>
-                  <FormLabel mt={5}>Expiry Date (MM/YY)</FormLabel>
-                  {/* separate dates into smaller grid */}
-                  <SimpleGrid columns={2} columnGap={3}>
-                    <GridItem colSpan={1}>
-                      <Input
-                        type="number"
-                        placeholder="MM"
-                        {...register("expDateMonth", {
-                          required: "Can't be blank",
-                          max: {
-                            value: 12,
-                            message: "Month is invalid",
-                          },
-                          maxLength: 2,
-                        })}
-                        isInvalid={checkIfInvalid("expDateMonth")}
-                      />
-                      <ErrorMessage
-                        errors={errors}
-                        name="expDateMonth"
-                        render={({ message }) => (
-                          <p className={styles.error}>{message}</p>
-                        )}
-                      />
-                    </GridItem>
-                    <GridItem colSpan={1}>
-                      <Input
-                        type="number"
-                        placeholder="YY"
-                        {...register("expDateYear", {
-                          required: "Can't be blank",
-                          min: {
-                            value: 22,
-                            message: "Year can't be less than current",
-                          },
-                          maxLength: 2,
-                        })}
-                        isInvalid={checkIfInvalid("expDateYear")}
-                      />
-                      <ErrorMessage
-                        errors={errors}
-                        name="expDateYear"
-                        render={({ message }) => (
-                          <p className={styles.error}>{message}</p>
-                        )}
-                      />
-                    </GridItem>
-                  </SimpleGrid>
-                </GridItem>
-                <GridItem colSpan={1}>
-                  <FormLabel mt={5}>CVC</FormLabel>
-                  <Input
-                    type="number"
-                    placeholder="e.g. 123"
-                    {...register("cvcNumber", {
-                      required: "Can't be blank",
-                      maxLength: 3,
-                      pattern: /^[0-9]{3}/,
-                    })}
-                    isInvalid={checkIfInvalid("cvcNumber")}
-                  />
-                  <ErrorMessage
-                    errors={errors}
-                    name="cvcNumber"
-                    render={({ message }) => (
-                      <p className={styles.error}>{message}</p>
-                    )}
-                  />
-                </GridItem>
-              </SimpleGrid>
-            </FormControl>
-            <Button
-              colorScheme="blackAlpha"
-              bg="black"
-              size="lg"
-              w="full"
-              mt={8}
-              type="submit"
-            >
-              Confirm
-            </Button>
-          </form>
-        </VStack>
+        <CardForm onSubmit={handleSubmit} />
       </Container>
     </Box>
   );
---
+++ src/components/CardForm.tsx
@@ -0,0 +1,165 @@
+import { useForm, FormProvider } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { CardInfo } from "../interfaces";
+
+import styles from "./CardDetails.module.css";
+
+import {
+  FormControl,
+  FormLabel,
+  VStack,
+  Input,
+  SimpleGrid,
+  GridItem,
+  Button,
+} from "@chakra-ui/react";
+
+function CardForm(onSubmit) {
+  const methods = useForm<CardInfo>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  function checkIfInvalid(value: string): boolean {
+    return Object.hasOwn(errors, value);
+  }
+
+  return (
+    <VStack w="full" h="full" p={10} spacing="24px">
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <FormLabel>Cardholder Name</FormLabel>
+            <Input
+              placeholder="e.g Jane Appleseed"
+              type="text"
+              {...register("name", {
+                required: "Please enter your name",
+              })}
+              isInvalid={checkIfInvalid("name")}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="name"
+              render={({ message }) => (
+                <p className={styles.error}>{message}</p>
+              )}
+            />
+            <FormLabel mt={5}>Card Number</FormLabel>
+            <Input
+              type="text"
+              placeholder="e.g. 1234 5678 9123 0000"
+              {...register("number", {
+                required: "Can't be blank",
+                pattern: {
+                  // value: /^[0-9]*\s*$/,
+                  value: /\d\s*/,
+                  message: "Wrong format, numbers only",
+                },
+                minLength: {
+                  value: 16,
+                  message: "Number is too short",
+                },
+                maxLength: 19,
+              })}
+              isInvalid={checkIfInvalid("number")}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="number"
+              render={({ message }) => (
+                <p className={styles.error}>{message}</p>
+              )}
+            />
+            <SimpleGrid columns={2} columnGap={6} rowGap={2}>
+              <GridItem colSpan={1}>
+                <FormLabel mt={5}>Expiry Date (MM/YY)</FormLabel>
+                {/* separate dates into smaller grid */}
+                <SimpleGrid columns={2} columnGap={3}>
+                  <GridItem colSpan={1}>
+                    <Input
+                      type="number"
+                      placeholder="MM"
+                      {...register("expDateMonth", {
+                        required: "Can't be blank",
+                        max: {
+                          value: 12,
+                          message: "Month is invalid",
+                        },
+                        maxLength: 2,
+                      })}
+                      isInvalid={checkIfInvalid("expDateMonth")}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="expDateMonth"
+                      render={({ message }) => (
+                        <p className={styles.error}>{message}</p>
+                      )}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <Input
+                      type="number"
+                      placeholder="YY"
+                      {...register("expDateYear", {
+                        required: "Can't be blank",
+                        min: {
+                          value: 22,
+                          message: "Year can't be less than current",
+                        },
+                        maxLength: 2,
+                      })}
+                      isInvalid={checkIfInvalid("expDateYear")}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="expDateYear"
+                      render={({ message }) => (
+                        <p className={styles.error}>{message}</p>
+                      )}
+                    />
+                  </GridItem>
+                </SimpleGrid>
+              </GridItem>
+              <GridItem colSpan={1}>
+                <FormLabel mt={5}>CVC</FormLabel>
+                <Input
+                  type="number"
+                  placeholder="e.g. 123"
+                  {...register("cvcNumber", {
+                    required: "Can't be blank",
+                    maxLength: 3,
+                    pattern: /^[0-9]{3}/,
+                  })}
+                  isInvalid={checkIfInvalid("cvcNumber")}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="cvcNumber"
+                  render={({ message }) => (
+                    <p className={styles.error}>{message}</p>
+                  )}
+                />
+              </GridItem>
+            </SimpleGrid>
+          </FormControl>
+          <Button
+            colorScheme="blackAlpha"
+            bg="black"
+            size="lg"
+            w="full"
+            mt={8}
+            type="submit"
+          >
+            Confirm
+          </Button>
+        </form>
+      </FormProvider>
+    </VStack>
+  );
+}
+
+export default CardForm;
