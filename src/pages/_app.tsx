30262d45869ef0caf387c03472db5893d65f9d0a
denizedaza <dazadenize@gmail.com>
2022-09-23 17:57:03 -0400

    refactor: fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 const theme = extendTheme({
